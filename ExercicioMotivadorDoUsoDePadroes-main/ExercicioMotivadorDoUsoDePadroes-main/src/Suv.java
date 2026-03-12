3bb8a50e4b49557e219df5ea886d041ff8f748f0
DanFreetz <77791341+DanFreetz@users.noreply.github.com>
2026-03-12 18:25:47 +0000

    Tentando denovo

--- ExercicioMotivadorDoUsoDePadroes-main/ExercicioMotivadorDoUsoDePadroes-main/src/Suv.java
+++
@@ -1,6 +0,0 @@
-public class Suv extends Carro{
-
-    public Suv(){
-        super("SUV", TipoCombustivel.FLEX, 8, 55);
-    }
-}
--- ExercicioMotivadorDoUsoDePadroes-main/ExercicioMotivadorDoUsoDePadroes-main/src/Utilitario.java
+++
@@ -1,6 +0,0 @@
-public class Utilitario extends Carro {
-    
-    public Utilitario(){
-        super("Utilitário",TipoCombustivel.DIESEL,5,70);
-    }
-}
